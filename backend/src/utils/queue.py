f2e7bdde58d62c843790ae8befb9b09f2dfc35b2
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2024-07-09 11:56:38 -0600

    format files

--- backend/src/utils/queue.py
+++ backend/src/utils/queue.py
@@ -5,10 +5,10 @@ from ..config.settings import settings
 
 async def print_task(queue: deque):
     print("Waiting for tasks...")
-    
+
     while True:
         if queue:
             print("Processing task in queue...")
             print(queue.popleft())
-            
+
         await asyncio.sleep(settings.ASYNCIO_DELAY)
