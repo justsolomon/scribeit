bc261622a18a652991071df8aa652b973ef87b70
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2024-08-06 11:38:09 -0600

    format files

--- backend/src/routes/video.py
+++ backend/src/routes/video.py
@@ -12,11 +12,15 @@ def video_upload_allowed(req: Request):
     return {"message": "Upload allowed"}
 
 
-@router.post("/video/upload", dependencies=[Depends(video_service.validate_content_length)])
+@router.post(
+    "/video/upload", dependencies=[Depends(video_service.validate_content_length)]
+)
 async def upload_video(req: Request, video: UploadFile):
     video_service.check_upload_allowed(req)
 
-    user_id = await video_service.add_video_to_queue(video, req.app.video_queue, req.app.pusher_client)
+    user_id = await video_service.add_video_to_queue(
+        video, req.app.video_queue, req.app.pusher_client
+    )
 
     return {
         "userId": f"{user_id}",
